924ed1087fea448c131a61c08a2d53b486a65aa8
lgiacalo <lgiacalo@student.42.fr>
2021-01-21 21:50:55 +0100

    clean eslint

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -24,7 +24,7 @@ const validateFunction = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty())
     return res.status(422).json({ errors: errors.array() });
-  next();
+  return next();
 };
 
 module.exports = {
--- utils/cron.js
+++ utils/cron.js
@@ -1,17 +1,17 @@
-const CronJob = require('cron').CronJob;
+const { CronJob } = require('cron');
 const { getInsightKeep, deleteInsightKeepId } = require('./requests_db');
 const { postAnnotate } = require('./requests_robotoff');
 
 // '0 */4 * * *' toutes les 4h
 // '*/2 * * * *' toutes les 2min
 const initCron = () => {
-  const job = new CronJob('0 */4 * * *', function () {
+  const job = new CronJob('0 */4 * * *', () => {
     getInsightKeep()
       .then((results) => {
-        results.forEach(({ insight_id, annotate }, id) => {
-          postAnnotate(insight_id, annotate)
+        results.forEach(({ insight_id: insightId, annotate }) => {
+          postAnnotate(insightId, annotate)
             .then(() => {
-              deleteInsightKeepId(insight_id);
+              deleteInsightKeepId(insightId);
             })
             .catch((err) => {
               console.log('err 1 :>> ', err.toString());
