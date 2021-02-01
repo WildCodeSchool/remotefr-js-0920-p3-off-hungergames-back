6e9ef0e5b7fd3d277c4fadf3e5044661b544f228
lgiacalo <lgiacalo@student.42.fr>
2021-02-01 12:52:38 +0100

    try with Pool + connectionString

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+const pg = require('pg');
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = true;
+}
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -11,22 +17,19 @@ const originsAllowed = isProd
 
 const nbConfirm = 3;
 
-const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 5432,
-  ssl: process.env.DB_SSL === 'true',
-  rejectUnauthorized: false,
-});
-
-// const db = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+// const db = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT || 5432,
+//   ssl: process.env.DB_SSL === 'true',
+//   rejectUnauthorized: false,
 // });
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 module.exports = {
   port: process.env.PORT || 5000,
